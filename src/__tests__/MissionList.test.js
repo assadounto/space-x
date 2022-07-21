3f6077818ff175be7e5e7795f6949836485e4b85
Shubh <shubh.scb@gmail.com>
2022-07-21 20:51:22 +0530

    TEST SUCCESS

--- src/__tests__/MissionList.test.js
+++ src/__tests__/MissionList.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import TestRenderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import store from '../redux/ConfigureStore';
-import Missionlist from '../components/Missionlist';
+import store from '../redux/configureStore';
+import Missionlist from '../components/MissionList';
 
 it('Rocket render correctly', () => {
   const Tree = TestRenderer.create(
--- src/__tests__/RocketList.test.js
+++ src/__tests__/RocketList.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import TestRenderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import store from '../redux/ConfigureStore';
+import store from '../redux/configureStore';
 import Rockets from '../components/RocketList';
 
 it('Rocket render correctly', () => {
